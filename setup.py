3f421ff20388c160c4d418af1868873f77037e6d
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-04-25 01:01:38 -0800

    First rumblings of a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='json_to_degree',
+    version='0.0.2-rc1',
+    description='Covert compatible JSON configs to DeBAM/DeTIM config.dat files',
+    url='https://github.com/fmuzf/meltmod_jsontodegree',
+    author='Lyman Gillispie',
+    author_email='lyman.gillispie@gmail.com',
+    packages=['json_to_degree', 'json_to_degree.test'],
+    license='MIT',
+    long_description=open('README.md').read(),
+)
